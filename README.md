ef07bcae2c7dbfe5aad06ad8ba371f0053a8cd85
kkndo2 <kkndo2a@gmail.com>
2017-04-18 17:20:39 +0900

    모노로그 테스트 코드 삽입

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # monolog
-모노로그 테스트용
+## 기본개념
+PSR-3을 준수하는 로그 패키지
+
+## 참고링크
+* https://www.lesstif.com/pages/viewpage.action?pageId=23757072
+* http://blog.appkr.kr/work-n-play/php-application-logging-to-elasticsearch-using-monolog/
--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
-phpinfo();
+require 'vendor/autoload.php';
+
+use \Monolog\Logger as Logger;
+use Monolog\Handler\StreamHandler;
+
+// 로거 채널 생성
+$log =  new Logger('name');
+
+// log/your.log 파일에 로그 생성. 로그 레벨은 Info
+$log->pushHandler(new StreamHandler('log/your.log', Logger::DEBUG));
+
+// add records to the log
+$log->addInfo('Info log');
+// Debug 는 Info 레벨보다 낮으므로 아래 로그는 출력되지 않음
+$log->addDebug('Debug log');
+$log->addError('Error log');
